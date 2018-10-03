0810d5619f8061d96670d940a2a36ee4666d8d27
Lucas Carvalho <lucasccorrea@gmail.com>
2018-10-03 16:01:05 -0300

    Finish Component ListBook

--- src/Books/BookBox.js
+++ src/Books/BookBox.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import { FormBook } from './FormBook';
 import { ListBook } from './ListBook';
+import $ from 'jquery';
+import { API_URL_BOOK } from '../common/ApiUrl';
 
 export default class BookBox extends Component {
+	constructor() {
+		super();
+		this.state = { listBook: [] };
+		this.loadListBook();
+	}
+	
+	loadListBook() {
+		$.ajax({
+			url: API_URL_BOOK,
+			dataType: 'json',
+			success: res => this.setState({listBook: res}),
+			error: error => console.error(error)
+		});
+	}
+
 	render() {
 		return (
-			<div>
+			<div className="content">
 				<div className="header">
 					<h1>Cadastro de livros</h1>
 				</div>
 				<FormBook />
-				<ListBook />
+				<ListBook listBook={this.state.listBook} />
 			</div>
 		)
 	}
--- src/Books/ListBook.js
+++ src/Books/ListBook.js
@@ -3,7 +3,26 @@ import React, { Component } from 'react';
 export class ListBook extends Component {
 	render() {
 		return (
-			<div>ListBook</div>
+			<div>
+				<table className="pure-table">
+					<thead>
+						<tr>
+							<th>Título</th>
+							<th>Preço</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							this.props.listBook.map(book => 
+								<tr key={book.id}>
+									<td>{ book.titulo }</td>
+									<td>{ book.preco }</td>
+								</tr>
+							)
+						}
+					</tbody>
+				</table>
+			</div>
 		)
 	}
 }
